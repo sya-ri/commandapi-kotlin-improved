be4cdf0ed09402bef6a9aa78dff43bac63c3b4b0
sya-ri <git@s7a.dev>
2024-04-13 15:19:03 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlinter)
+    alias(libs.plugins.shadow)
+}
+
+group = "dev.s7a"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
