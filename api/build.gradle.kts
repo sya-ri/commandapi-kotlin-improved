d770c40b7a1a4615a4350f3fdadfb9ac280df6f5
sya-ri <git@s7a.dev>
2024-04-14 00:07:40 +0900

    chore: Fix publish task

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,73 +1,85 @@
 plugins {
+    alias(libs.plugins.dokka)
     `maven-publish`
     signing
 }
 
 subprojects {
-    apply(plugin = "maven-publish")
-    apply(plugin = "signing")
+    afterEvaluate {
+        apply(plugin = "maven-publish")
+        apply(plugin = "signing")
+        apply(plugin = libs.plugins.dokka.get().pluginId)
 
-    version = "1.0.0"
+        version = "1.0.0"
 
-    val sourceJar by tasks.registering(Jar::class) {
-        archiveClassifier.set("sources")
-        from(sourceSets["main"].allSource)
-    }
+        val sourceJar by tasks.registering(Jar::class) {
+            archiveClassifier.set("sources")
+            from(sourceSets["main"].allSource)
+        }
 
-    publishing {
-        repositories {
-            maven {
-                url =
-                    uri(
-                        if (version.toString().endsWith("SNAPSHOT")) {
-                            "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-                        } else {
-                            "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-                        },
-                    )
-                credentials {
-                    username = properties["sonatypeUsername"].toString()
-                    password = properties["sonatypePassword"].toString()
+        val javadocJar by tasks.registering(Jar::class) {
+            dependsOn(tasks.dokkaJavadoc)
+
+            archiveClassifier.set("javadoc")
+            from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
+        }
+
+        publishing {
+            repositories {
+                maven {
+                    url =
+                        uri(
+                            if (version.toString().endsWith("SNAPSHOT")) {
+                                "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+                            } else {
+                                "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+                            },
+                        )
+                    credentials {
+                        username = properties["sonatypeUsername"].toString()
+                        password = properties["sonatypePassword"].toString()
+                    }
                 }
             }
-        }
-        publications {
-            register<MavenPublication>("maven") {
-                groupId = "dev.s7a"
-                artifactId = "commandapi-${project.name}-kotlin-improved"
-                from(components["kotlin"])
-                artifact(sourceJar.get())
-                pom {
-                    name.set("commandapi-${project.name}-kotlin-improved")
-                    description.set("Improve CommandAPI Kotlin DSL")
-                    url.set("https://github.com/sya-ri/commandapi-kotlin-improved")
-
-                    licenses {
-                        license {
-                            name.set("MIT License")
-                            url.set("https://github.com/sya-ri/commandapi-kotlin-improved/blob/master/LICENSE")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("sya-ri")
-                            name.set("sya-ri")
-                            email.set("contact@s7a.dev")
-                        }
-                    }
-                    scm {
+            publications {
+                register<MavenPublication>("maven") {
+                    groupId = "dev.s7a"
+                    artifactId = "commandapi-${project.name}-kotlin-improved"
+                    from(components["kotlin"])
+                    artifact(sourceJar.get())
+                    artifact(javadocJar.get())
+                    pom {
+                        name.set("commandapi-${project.name}-kotlin-improved")
+                        description.set("Improve CommandAPI Kotlin DSL")
                         url.set("https://github.com/sya-ri/commandapi-kotlin-improved")
+
+                        licenses {
+                            license {
+                                name.set("MIT License")
+                                url.set("https://github.com/sya-ri/commandapi-kotlin-improved/blob/master/LICENSE")
+                            }
+                        }
+                        developers {
+                            developer {
+                                id.set("sya-ri")
+                                name.set("sya-ri")
+                                email.set("contact@s7a.dev")
+                            }
+                        }
+                        scm {
+                            url.set("https://github.com/sya-ri/commandapi-kotlin-improved")
+                        }
                     }
                 }
             }
         }
-    }
 
-    signing {
-        val key = properties["signingKey"]?.toString()?.replace("\\n", "\n")
-        val password = properties["signingPassword"]?.toString()
+        signing {
+            val key = properties["signingKey"]?.toString()?.replace("\\n", "\n")
+            val password = properties["signingPassword"]?.toString()
 
-        useInMemoryPgpKeys(key, password)
-        sign(publishing.publications["maven"])
+            useInMemoryPgpKeys(key, password)
+            sign(publishing.publications["maven"])
+        }
     }
 }
